9e06252481d269993e8420612b70c8e4980e30e2
Cesar <cesar.cervantes9416@gmail.com>
2019-06-09 08:19:23 -0700

    Renderizando imagenes
    Se agrego la funcionalidad para renderizar las imágenes en la vista "index.hbs"
    
    -Se utilizo el atributo virtual uniqueId

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,12 @@
 const ctrl = {};
+const {Image} = require('../models');
 
-ctrl.index = (req,res) =>{
-    res.render('index')
+ctrl.index = async (req,res) =>{
+    //Consulta para obtener imagenes
+    const images = await Image.find().sort({
+        timestamp:-1
+    });
+    res.render('index',{images})
 };
 
 module.exports = ctrl;
